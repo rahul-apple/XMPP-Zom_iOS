2e103367818e3ff31554ae2c280638f03caa1626
rahul-apple <hijaz.apple@gmail.com>
2017-03-02 14:42:31 +0530

    theme change apihandler update

--- Zom/Zom/VROChatAPIHandler.h
+++ Zom/Zom/VROChatAPIHandler.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
 
 @interface VROChatAPIHandler : NSObject
 
--- Zom/Zom/VROChatAPIHandler.m
+++ Zom/Zom/VROChatAPIHandler.m
@@ -7,6 +7,17 @@
 //
 
 #import "VROChatAPIHandler.h"
+#import <AFNetworking/AFNetworking.h>
+
+#define REG_URL @"https://vrocloud.com/vro_v3/users/"
+#define BASE_URL @"https://vrocloud.com/production/v1/"
+
+@interface VROChatAPIHandler ()
+@property (nonatomic, strong) NSOperationQueue *apiOperationQueue;
+@property (nonatomic ,strong)AFHTTPSessionManager *manager;
+
+@end
+
 
 @implementation VROChatAPIHandler
 
